75a1b368b4fc43f46983befd133405483011e105
Jethro Kuan <jethrokuan95@gmail.com>
2020-05-29 11:19:16 +0800

    Add loihi benchmark code

--- snn_models/baseline_snn.py
+++ snn_models/baseline_snn.py
@@ -45,21 +45,21 @@ class SlayerMLP(torch.nn.Module):
 #         self.spike_trains = [spike_1, spike_2]
 #         return spike_3
 
-# class SlayerLoihiMLP(torch.nn.Module):
-#     '''
-#     2 layer MLP based on SLAYER used for tactile data
-#     '''
-#     def __init__(self, params, input_size, hidden_size, output_size):
-#         super(SlayerLoihiMLP, self).__init__()
-#         self.output_size = output_size
-#         self.slayer = spikeLayer(params["neuron"], params["simulation"])
-#         self.fc1 = self.slayer.dense(input_size, hidden_size)
-#         self.fc2 = self.slayer.dense(hidden_size, output_size)
+class SlayerLoihiMLP(torch.nn.Module):
+    '''
+    2 layer MLP based on SLAYER used for tactile data
+    '''
+    def __init__(self, params, input_size, hidden_size, output_size):
+        super(SlayerLoihiMLP, self).__init__()
+        self.output_size = output_size
+        self.slayer = spikeLayer(params["neuron"], params["simulation"])
+        self.fc1 = self.slayer.dense(input_size, hidden_size)
+        self.fc2 = self.slayer.dense(hidden_size, output_size)
 
-#     def forward(self, spike_input):
-#         spike_1 = self.slayer.spikeLoihi(self.slayer.psp(self.fc1(spike_input)))
-#         spike_1 = self.slayer.delayShift(spike_1, 1)
-#         spike_output = self.slayer.spikeLoihi(self.slayer.psp(self.fc2(spike_1)))
-#         spike_output = self.slayer.delayShift(spike_output, 1)
-#         self.spike_trains = [spike_1]
-#         return spike_output
+    def forward(self, spike_input):
+        spike_1 = self.slayer.spikeLoihi(self.slayer.psp(self.fc1(spike_input)))
+        spike_1 = self.slayer.delayShift(spike_1, 1)
+        spike_output = self.slayer.spikeLoihi(self.slayer.psp(self.fc2(spike_1)))
+        spike_output = self.slayer.delayShift(spike_output, 1)
+        self.spike_trains = [spike_1]
+        return spike_output
