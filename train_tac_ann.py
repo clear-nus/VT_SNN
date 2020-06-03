1b02d9f660cc051cb7b66ca1b7ce7ae13fc093be
tasbolat1 <ttaunyazov@nu.edu.kz>
2020-06-03 12:27:52 +0800

    ANN cleanup

--- train_tac_ann.py
+++ train_tac_ann.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[17]:
-
-
 import torch
 import numpy as np
 from pathlib import Path
@@ -15,22 +9,15 @@ import argparse
 from torch import nn
 import matplotlib.pyplot as plt
 
-
-# In[2]:
-
-
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
-
 log = logging.getLogger()
 
-
-parser = argparse.ArgumentParser("Train model.")
+parser = argparse.ArgumentParser("Train Tactile MLP-GRU model.")
 parser.add_argument("--epochs", type=int, help="Number of epochs.", required=True)
 parser.add_argument("--data_dir", type=str, help="Path to data.", required=True)
 parser.add_argument(
     "--checkpoint_dir", type=str, help="Path for saving checkpoints.", required=True
 )
-
 parser.add_argument("--lr", type=float, help="Learning rate.", required=True)
 parser.add_argument(
     "--sample_file", type=int, help="Sample number to train from.", required=True
@@ -38,165 +25,112 @@ parser.add_argument(
 parser.add_argument(
     "--hidden_size", type=int, help="Size of hidden layer.", required=True
 )
-parser.add_argument(
-    "--batch_size", type=int, help="Batch Size.", required=True
-)
-parser.add_argument(
-    "--output_size", type=int, help="Number of classes.", default=20
-)
+parser.add_argument("--batch_size", type=int, help="Batch Size.", required=True)
+parser.add_argument("--output_size", type=int, help="Number of classes.", required=True)
 
 args = parser.parse_args()
 
-output_size = args.output_size
-
-# In[4]:
-
-
-# class FLAGS():
-#     def __init__(self):
-#         self.data_dir = '/home/tasbolat/some_python_examples/data_VT_SNN/'
-#         self.batch_size = 8
-#         self.sample_file = 1
-#         self.lr = 0.01
-#         self.epochs = 100
-# args = FLAGS()
-
-
-# In[5]:
-
-
-device = torch.device("cuda:2")
+device = torch.device("cuda")
 writer = SummaryWriter(".")
 
-
-# In[6]:
-
-
 train_dataset = ViTacDataset(
-    path=args.data_dir, sample_file=f"train_80_20_{args.sample_file}.txt", output_size=output_size
+    path=args.data_dir,
+    sample_file=f"train_80_20_{args.sample_file}.txt",
+    output_size=args.output_size,
 )
 train_loader = DataLoader(
     dataset=train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4
 )
 test_dataset = ViTacDataset(
-    path=args.data_dir, sample_file=f"test_80_20_{args.sample_file}.txt", output_size=output_size
+    path=args.data_dir,
+    sample_file=f"test_80_20_{args.sample_file}.txt",
+    output_size=args.output_size,
 )
 test_loader = DataLoader(
     dataset=test_dataset, batch_size=args.batch_size, shuffle=True, num_workers=4
 )
 
 
-
-
-class MLP_LSTM(nn.Module):
-
+class MLP_GRU(nn.Module):
     def __init__(self):
-        super(MLP_LSTM, self).__init__()
-        self.input_size = 156
+        super(MLP_GRU, self).__init__()
+        self.input_size = 156  # Tactile has 156 taxels
         self.hidden_dim = args.hidden_size
-        self.num_layers = 1
 
-        # Define the LSTM layer
-        self.gru = nn.GRU(self.input_size, self.hidden_dim, self.num_layers)
-
-        # Define the output layer
-        self.fc = nn.Linear(self.hidden_dim, output_size)
-        
-        #self.fc_mlp = nn.Linear(156, self.input_size)
+        self.gru = nn.GRU(self.input_size, self.hidden_dim, 1)
+        self.fc = nn.Linear(self.hidden_dim, args.output_size)
 
     def forward(self, input_data):
-
         gru_in = input_data
-        
-        gru_in = gru_in.permute(1,0,2)
+        gru_in = gru_in.permute(1, 0, 2)
         gru_out, self.hidden = self.gru(gru_in)
-        
-        
-        # Only take the output from the final timetep
-        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
         y_pred = self.fc(gru_out[-1, :, :])
-        
-        #print(y_pred.shape)
         return y_pred
 
 
-
-def _save_model(epoch, loss):
+def _save_model(epoch):
     log.info(f"Writing model at epoch {epoch}...")
-    checkpoint_path = (
-        Path(args.checkpoint_dir) / f"weights-{epoch:03d}-{loss:0.3f}.pt"
-    )
+    checkpoint_path = Path(args.checkpoint_dir) / f"weights-{epoch:03d}.pt"
     torch.save(net.state_dict(), checkpoint_path)
 
 
-net = MLP_LSTM().to(device)
-# Create snn loss instance.
-criterion = nn.CrossEntropyLoss()
-# Define optimizer module.
-optimizer = torch.optim.RMSprop(
-    net.parameters(), lr=args.lr) # 0.0001
-
-
-for epoch in range(1, args.epochs+1):
-    # Training loop.
+def _train(epoch):
     net.train()
     correct = 0
     batch_loss = 0
-    train_acc = 0
-    for i, (in_tact, _, _, label) in enumerate(train_loader, 0):
-
+    for i, (in_tact, _, _, label) in enumerate(train_loader):
         in_tact = in_tact.to(device)
         in_tact = in_tact.squeeze()
-        in_tact = in_tact.permute(0,2,1)
+        in_tact = in_tact.permute(0, 2, 1)
         label = label.to(device)
-        # Forward pass of the network.
         out_tact = net.forward(in_tact)
-        #print(out_tact.shape)
-        # Calculate loss.
-        #print(label.shape)
         loss = criterion(out_tact, label)
-        #print(loss)
 
         batch_loss += loss.cpu().data.item()
-        # Reset gradients to zero.
         optimizer.zero_grad()
-        # Backward pass of the network.
         loss.backward()
-        # Update weights.
         optimizer.step()
 
         _, predicted = torch.max(out_tact.data, 1)
         correct += (predicted == label).sum().item()
 
-    # Reset training stats.
-    train_acc = correct/len(train_loader.dataset)
-    writer.add_scalar("loss/train", batch_loss/len(train_loader.dataset), epoch)
+    train_acc = correct / len(train_loader.dataset)
+    writer.add_scalar("loss/train", batch_loss / len(train_loader.dataset), epoch)
     writer.add_scalar("acc/train", train_acc, epoch)
 
-    # testing
+
+def _test(epoch):
+    """Testing loop for MLP-GRU model."""
     net.eval()
     correct = 0
     batch_loss = 0
-    test_acc = 0
     with torch.no_grad():
-        for i, (in_tact, _, _, label) in enumerate(test_loader, 0):
+        for i, (in_tact, _, _, label) in enumerate(test_loader):
             in_tact = in_tact.to(device)
             in_tact = in_tact.squeeze()
-            in_tact = in_tact.permute(0,2,1)
+            in_tact = in_tact.permute(0, 2, 1)
 
-            # Forward pass of the network.
             out_tact = net.forward(in_tact)
             label = label.to(device)
             _, predicted = torch.max(out_tact.data, 1)
             correct += (predicted == label).sum().item()
-            # Calculate loss.
+
             loss = criterion(out_tact, label)
             batch_loss += loss.cpu().data.item()
 
-    test_acc = correct/len(test_loader.dataset)
+    test_acc = correct / len(test_loader.dataset)
 
-    writer.add_scalar("loss/test", batch_loss/len(test_loader.dataset), epoch)
+    writer.add_scalar("loss/test", batch_loss / len(test_loader.dataset), epoch)
     writer.add_scalar("acc/test", test_acc, epoch)
 
-    if epoch%100 == 0:
-        _save_model(epoch, batch_loss/len(test_loader.dataset))
+
+net = MLP_GRU().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.RMSprop(net.parameters(), lr=args.lr)
+
+for epoch in range(1, args.epochs + 1):
+    _train(epoch)
+    if epoch % 50 == 0:
+        _test(epoch)
+    if epoch % 100 == 0:
+        _save_model(epoch)
--- train_vis_ann.py
+++ train_vis_ann.py
@@ -1,7 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-
 from datetime import datetime
 import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
@@ -14,19 +10,9 @@ from pathlib import Path
 import logging
 import argparse
 from torch.utils.tensorboard import SummaryWriter
-
-
-# class FLAGS():
-#     def __init__(self):
-#         self.data_dir = '/home/tasbolat/some_python_examples/data_VT_SNN/'
-#         self.batch_size = 8
-#         self.sample_file = 1
-#         self.lr = 0.0001
-#         self.epochs = 400
-# args = FLAGS()
+from dataset import ViTacVisDataset
 
 logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
-
 log = logging.getLogger()
 
 
@@ -36,7 +22,6 @@ parser.add_argument("--data_dir", type=str, help="Path to data.", required=True)
 parser.add_argument(
     "--checkpoint_dir", type=str, help="Path for saving checkpoints.", required=True
 )
-
 parser.add_argument("--lr", type=float, help="Learning rate.", required=True)
 parser.add_argument(
     "--sample_file", type=int, help="Sample number to train from.", required=True
@@ -44,170 +29,120 @@ parser.add_argument(
 parser.add_argument(
     "--hidden_size", type=int, help="Size of hidden layer.", required=True
 )
-parser.add_argument(
-    "--batch_size", type=int, help="Batch Size.", required=True
-)
-parser.add_argument(
-    "--output_size", type=int, help="Number of classes.", default=20
-)
+parser.add_argument("--batch_size", type=int, help="Batch Size.", required=True)
+parser.add_argument("--output_size", type=int, help="Number of classes.", required=True)
 
 args = parser.parse_args()
 
-# Dataset definition
-class ViTacDataset(Dataset):
-    def __init__(self, datasetPath, sampleFile):
-        self.path = datasetPath 
-        self.samples = np.loadtxt(sampleFile).astype('int')
-        self.vis = torch.load(Path(self.path) / "ds_vis.pt")
+train_dataset = ViTacVisDataset(
+    path=args.data_dir,
+    sample_file=f"train_80_20_{args.sample_file}.txt",
+    output_size=args.output_size,
+    spike=False,
+)
+train_loader = DataLoader(
+    dataset=train_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8
+)
 
-    def __getitem__(self, index):
-        inputIndex  = self.samples[index, 0]
-        classLabel  = self.samples[index, 1]
-        return self.vis[inputIndex], classLabel
-
-    def __len__(self):
-        return self.samples.shape[0]
-
-
-# In[4]:
-
-
-# Dataset and dataLoader instances.
-split_list = ['80_20_1','80_20_2','80_20_3','80_20_4','80_20_5']
-
-
-#data_dir = '/home/tasbolat/some_python_examples/data_VT_SNN/'
-
-
-#sample_file = 1
-trainingSet = ViTacDataset(datasetPath = args.data_dir, sampleFile = args.data_dir + "/train_" + split_list[args.sample_file-1] + ".txt")
-train_loader = DataLoader(dataset=trainingSet, batch_size = args.batch_size, shuffle=False, num_workers=8)
- 
-testingSet = ViTacDataset(datasetPath = args.data_dir, sampleFile  = args.data_dir + "/test_" + split_list[args.sample_file-1] + ".txt")
-test_loader = DataLoader(dataset=testingSet, batch_size = args.batch_size, shuffle=False, num_workers=8)
-
+test_dataset = ViTacVisDataset(
+    path=args.data_dir,
+    sample_file=f"test_80_20_{args.sample_file}.txt",
+    output_size=args.output_size,
+    spike=False,
+)
+test_loader = DataLoader(
+    dataset=test_dataset, batch_size=args.batch_size, shuffle=False, num_workers=8
+)
 
 
 class Vis_MLP_GRU(nn.Module):
-
     def __init__(self):
         super(Vis_MLP_GRU, self).__init__()
         self.input_size = 1000
-        self.hidden_dim = args.hidden_size #32
-        self.batch_size = 8
-        self.num_layers = 1
+        self.hidden_dim = args.hidden_size
+        self.batch_size = args.batch_size
 
         # Define the LSTM layer
-        self.gru = nn.GRU(self.input_size, self.hidden_dim, self.num_layers)
+        self.gru = nn.GRU(self.input_size, self.hidden_dim, 1)
 
         # Define the output layer
         self.fc = nn.Linear(self.hidden_dim, args.output_size)
-        
-        #self.pool_vis = nn.AvgPool3d((1,5,5), padding=0, stride=(1,7,7))
-        self.fc_vis = nn.Linear(63*50*2, self.input_size)
 
+        # self.pool_vis = nn.AvgPool3d((1,5,5), padding=0, stride=(1,7,7))
+        self.fc_vis = nn.Linear(63 * 50 * 2, self.input_size)
 
     def forward(self, in_vis):
-        in_vis = in_vis.reshape([in_vis.shape[0], in_vis.shape[-1], 50*63*2])
-        #print('in vis:', in_vis.shape)
-        embeddings = self.fc_vis(in_vis).permute(1,0,2)
-        #print('embeddings:', embeddings.shape)
-                
-        # GRU input type: (seq_len, batch, input_size)
+        in_vis = in_vis.reshape([in_vis.shape[0], in_vis.shape[-1], 50 * 63 * 2])
+        embeddings = self.fc_vis(in_vis).permute(1, 0, 2)
         out, hidden = self.gru(embeddings)
-        out = out.permute(1,0,2)        
-        
-        # Only take the output from the final timetep
-        #print('out:', out.shape)
+        out = out.permute(1, 0, 2)
+
         y_pred = self.fc(out[:, -1, :])
-        
+
         return y_pred
 
 
-# In[74]:
-
-device = torch.device("cuda:2")
+device = torch.device("cuda")
 writer = SummaryWriter(".")
 
+net = Vis_MLP_GRU().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.RMSprop(net.parameters(), lr=args.lr)
 
-def _save_model(epoch, loss):
+
+def _save_model(epoch):
     log.info(f"Writing model at epoch {epoch}...")
-    checkpoint_path = (
-        Path(args.checkpoint_dir) / f"weights-{epoch:03d}-{loss:0.3f}.pt"
-    )
+    checkpoint_path = Path(args.checkpoint_dir) / f"weights-{epoch:03d}.pt"
     torch.save(net.state_dict(), checkpoint_path)
 
 
-
-train_accs = []
-test_accs = []
-train_loss = []
-test_loss = []
-
-
-net = Vis_MLP_GRU().to(device)
-# Create snn loss instance.
-criterion = nn.CrossEntropyLoss()
-# Define optimizer module.
-optimizer = torch.optim.RMSprop(net.parameters(), lr = args.lr)
-
-
-for epoch in range(1, args.epochs+1):
-    # Training loop.
+def _train(epoch):
     net.train()
     correct = 0
     batch_loss = 0
     train_acc = 0
-    for i, (in_vis, label) in enumerate(train_loader, 0):
-
+    for i, (in_vis, _, label) in enumerate(train_loader):
         in_vis = in_vis.to(device)
-        #in_vis = in_vis.squeeze().permute(0,1,4,2,3)
-        #print(in_vis.shape)
         label = label.to(device)
-        # Forward pass of the network.
-        out_tact = net.forward(in_vis)
-        # Calculate loss.
-        loss = criterion(out_tact, label)
-        #print(loss)
+        out_vis = net.forward(in_vis)
+        loss = criterion(out_vis, label)
 
         batch_loss += loss.cpu().data.item()
-        # Reset gradients to zero.
         optimizer.zero_grad()
-        # Backward pass of the network.
         loss.backward()
-        # Update weights.
         optimizer.step()
 
-        _, predicted = torch.max(out_tact.data, 1)
+        _, predicted = torch.max(out_vis.data, 1)
         correct += (predicted == label).sum().item()
 
-    # Reset training stats.
-    train_acc = correct/len(train_loader.dataset)
-    writer.add_scalar("loss/train", batch_loss/len(train_loader.dataset), epoch)
+    train_acc = correct / len(train_loader.dataset)
+    writer.add_scalar("loss/train", batch_loss / len(train_loader.dataset), epoch)
     writer.add_scalar("acc/train", train_acc, epoch)
 
-    # testing
+
+def _test(epoch):
     net.eval()
     correct = 0
     batch_loss = 0
     test_acc = 0
     with torch.no_grad():
-        for i, (in_vis, label) in enumerate(test_loader, 0):
+        for i, (in_vis, _, label) in enumerate(test_loader, 0):
             in_vis = in_vis.to(device)
-            #in_vis = in_vis.squeeze().permute(0,1,4,2,3)
-
-            # Forward pass of the network.
-            out_tact = net.forward(in_vis)
+            out_vis = net.forward(in_vis)
             label = label.to(device)
-            _, predicted = torch.max(out_tact.data, 1)
+            _, predicted = torch.max(out_vis.data, 1)
             correct += (predicted == label).sum().item()
-            # Calculate loss.
-            loss = criterion(out_tact, label)
+            loss = criterion(out_vis, label)
             batch_loss += loss.cpu().data.item()
 
-    test_acc = correct/len(test_loader.dataset)
-    writer.add_scalar("loss/test", batch_loss/len(test_loader.dataset), epoch)
+    test_acc = correct / len(test_loader.dataset)
+    writer.add_scalar("loss/test", batch_loss / len(test_loader.dataset), epoch)
     writer.add_scalar("acc/test", test_acc, epoch)
 
-    if epoch%100 == 0:
-        _save_model(epoch, batch_loss/len(test_loader.dataset))
+
+for epoch in range(1, args.epochs + 1):
+    _train(epoch)
+    if epoch % 50 == 0:
+        _test(epoch)
+    if epoch % 100 == 0:
+        _save_model(epoch)
