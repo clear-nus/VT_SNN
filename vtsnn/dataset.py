5dead74ae58d7b7f4ffe8886eec5bde9b8d3c3de
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-07 23:22:59 +0800

    update

--- vtsnn/dataset.py
+++ vtsnn/dataset.py
@@ -1,12 +1,12 @@
+"""Datasets definitions for VT-SNN.
+
+Each dataset should return an output of shape (data, target, label).
+"""
+
 import torch
 from torch.utils.data import Dataset
 import numpy as np
 from pathlib import Path
-import slayerSNN as snn
-
-# Example Usage:
-# dataset = ViTacDataset(path="20200124170823_grasp_lift_hold/",
-#                        sample_file="train_80_20_4.txt")
 
 
 class ViTacDataset(Dataset):
@@ -15,7 +15,7 @@ class ViTacDataset(Dataset):
         self.output_size = output_size
         sample_file = Path(path) / sample_file
         self.samples = np.loadtxt(sample_file).astype("int")
-        
+
         self.rectangular = rectangular
         if rectangular:
             self.right_tact = torch.load(Path(path) / "tac_right.pt")
@@ -29,21 +29,19 @@ class ViTacDataset(Dataset):
         class_label = self.samples[index, 1]
         target_class = torch.zeros((self.output_size, 1, 1, 1))
         target_class[class_label, ...] = 1
-    
+
         if self.rectangular:
             return (
-                self.right_tact[input_index],
-                self.left_tact[input_index],
+                (self.right_tact[input_index], self.left_tact[input_index]),
                 target_class,
                 class_label,
             )
-        else:
-            return (
-                self.tact[input_index],
-                torch.tensor(0),
-                target_class,
-                class_label,
-            )
+
+        return (
+            (self.tact[input_index]),
+            target_class,
+            class_label,
+        )
 
     def __len__(self):
         return self.samples.shape[0]
@@ -55,12 +53,13 @@ class ViTacVisDataset(Dataset):
         sample_file = Path(path) / sample_file
         self.samples = np.loadtxt(sample_file).astype("int")
         self.output_size = output_size
-        
-        self.spike=spike
+
+        self.spike = spike
         if spike:
             self.vis = torch.load(Path(path) / "ds_vis.pt")
         else:
             self.vis = torch.load(Path(path) / "ds_vis_non_spike.pt")
+
     def __getitem__(self, index):
         input_index = self.samples[index, 0]
         class_label = self.samples[index, 1]
@@ -68,25 +67,29 @@ class ViTacVisDataset(Dataset):
         target_class[class_label, ...] = 1
 
         return (
-            self.vis[input_index],
+            (self.vis[input_index]),
             target_class,
             class_label,
         )
+
     def __len__(self):
         return self.samples.shape[0]
 
+
 class ViTacMMDataset(Dataset):
-    def __init__(self, path, sample_file, output_size, spike=True, rectangular=False):
+    def __init__(
+        self, path, sample_file, output_size, spike=True, rectangular=False
+    ):
         self.path = path
         self.samples = np.loadtxt(Path(path) / sample_file).astype("int")
         self.output_size = output_size
-        
-        self.spike=spike
+
+        self.spike = spike
         if spike:
             self.ds_vis = torch.load(Path(path) / "ds_vis.pt")
         else:
             self.ds_vis = torch.load(Path(path) / "ds_vis_non_spike.pt")
-            
+
         self.rectangular = rectangular
         if rectangular:
             self.right_tact = torch.load(Path(path) / "tac_right.pt")
@@ -103,18 +106,20 @@ class ViTacMMDataset(Dataset):
 
         if self.rectangular:
             return (
-                self.right_tact[input_index],
-                self.left_tact[input_index],
-                self.ds_vis[input_index],
-                class_label,
-            )
-        else:
-            return (
-                self.tact[input_index],
-                self.ds_vis[input_index],
+                (
+                    self.right_tact[input_index],
+                    self.left_tact[input_index],
+                    self.ds_vis[input_index],
+                ),
                 target_class,
                 class_label,
             )
+
+        return (
+            (self.tact[input_index], self.ds_vis[input_index]),
+            target_class,
+            class_label,
+        )
 
     def __len__(self):
         return self.samples.shape[0]
--- vtsnn/models/snn.py
+++ vtsnn/models/snn.py
@@ -4,8 +4,7 @@ from slayerSNN import loihi as spikeLayer
 
 
 class SlayerMLP(torch.nn.Module):
-    """
-    2-layer MLP based on SLAYER used for tactile data."""
+    """2-layer MLP based on SLAYER used for tactile data."""
 
     def __init__(self, params, input_size, hidden_size, output_size):
         super(SlayerMLP, self).__init__()
@@ -13,49 +12,52 @@ class SlayerMLP(torch.nn.Module):
         self.slayer = snn.layer(params["neuron"], params["simulation"])
         self.fc1 = self.slayer.dense(input_size, hidden_size)
         self.fc2 = self.slayer.dense(hidden_size, output_size)
-        self.spike_trains = None
 
     def forward(self, spike_input):
         spike_1 = self.slayer.spike(self.slayer.psp(self.fc1(spike_input)))
         spike_output = self.slayer.spike(self.slayer.psp(self.fc2(spike_1)))
-        self.spike_trains = [spike_1]
         return spike_output
 
 
 class EncoderVis(torch.nn.Module):
-    def __init__(self, netParams, output_size):
+    def __init__(self, params, input_size, output_size):
         super(EncoderVis, self).__init__()
-        self.slayer = snn.layer(netParams["neuron"], netParams["simulation"])
-        self.fc1 = self.slayer.dense((50, 63, 2), output_size)
-        self.spike_trains = None
+        self.slayer = snn.layer(params["neuron"], params["simulation"])
+        self.fc1 = self.slayer.dense(input_size, output_size)
 
-    def forward(self, downsampled):
-        spike_output = self.slayer.spike(self.fc1(self.slayer.psp(downsampled)))
-        self.spike_trains = []
+    def forward(self, spike_input):
+        spike_output = self.slayer.spike(self.fc1(self.slayer.psp(spike_input)))
         return spike_output
 
 
 class EncoderTact(torch.nn.Module):
-    def __init__(self, netParams, output_size):
+    def __init__(self, params, input_size, output_size):
         super(EncoderTact, self).__init__()
-        slayer = snn.layer(netParams["neuron"], netParams["simulation"])
-        self.slayer = slayer
-        self.fc1 = slayer.dense(156, output_size)
+        self.slayer = snn.layer(params["neuron"], params["simulation"])
+        self.fc1 = self.slayer.dense(input_size, output_size)
 
-    def forward(self, spikeInput):
-        spikeLayer1 = self.slayer.spike(self.fc1(self.slayer.psp((spikeInput))))
-        self.spike_trains = []
-        return spikeLayer1
+    def forward(self, spike_input):
+        spike_output = self.slayer.spike(
+            self.fc1(self.slayer.psp((spike_input)))
+        )
+        return spike_output
 
 
 class SlayerMM(torch.nn.Module):
-    def __init__(self, netParams, output_size):
+    def __init__(
+        self,
+        params,
+        tact_input_size,
+        vis_input_size,
+        tact_output_size,
+        vis_output_size,
+        output_size,
+    ):
         super(SlayerMM, self).__init__()
-        slayer = snn.layer(netParams["neuron"], netParams["simulation"])
-        self.tactile = EncoderTact(netParams, 50)
-        self.vision = EncoderVis(netParams, 10)
-        self.slayer = slayer
-        self.fc1 = slayer.dense(60, output_size)
+        self.slayer = snn.layer(params["neuron"], params["simulation"])
+        self.tactile = EncoderTact(params, tact_input_size, tact_output_size)
+        self.vision = EncoderVis(params, vis_input_size, vis_output_size)
+        self.fc1 = self.slayer.dense(60, output_size)
 
     def forward(self, spikeInputTact, spikeInputVis):
         spikeLayer1 = self.tactile(spikeInputTact)
