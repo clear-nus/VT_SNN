792e0ed11bfe77e7a93ffe5df6143d042040b467
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-11 21:02:57 +0800

    combine preprocess to produce rect and downsampled

--- vtsnn/preprocess.py
+++ vtsnn/preprocess.py
@@ -19,6 +19,14 @@ python vtsnn/preprocess.py \
   --slip 0
 """
 
+import argparse
+import torch
+import numpy as np
+from pathlib import Path
+import slayerSNN as snn
+import torch.nn.functional as F
+import glob
+
 import pandas as pd
 import numpy as np
 import os
@@ -31,7 +39,6 @@ from joblib import Parallel, delayed
 from pathlib import Path
 
 from collections import namedtuple
-import argparse
 
 Trajectory = namedtuple(
     "Trajectory",
@@ -67,6 +74,9 @@ parser.add_argument(
     type=int,
     help="Number of samples per class.",
     required=True,
+)
+parser.add_argument(
+    "--network_config", type=str, help="Configuration to use.", required=True
 )
 parser.add_argument(
     "--slip",
@@ -417,3 +427,247 @@ for split in range(args.num_splits):
         fmt="%d",
         delimiter="\t",
     )
+
+# Reprocess into compact .pt format
+
+
+class SumPool(torch.nn.Module):  # outputs spike trains
+    def __init__(self, params):
+        super(SumPool, self).__init__()
+        self.slayer = snn.layer(params["neuron"], params["simulation"])
+        self.pool = self.slayer.pool(4)
+
+    def forward(self, input_data):
+        spike_out = self.slayer.spike(self.pool(self.slayer.psp(input_data)))
+        return spike_out
+
+
+class AvgPool(torch.nn.Module):
+    def __init__(self):
+        super(AvgPool, self).__init__()
+        self.pool = torch.nn.AvgPool3d(
+            (1, 4, 4), padding=[0, 1, 1], stride=(1, 4, 4)
+        )
+
+    def forward(self, input_data):
+        out_data = F.relu(self.pool(input_data))
+        return out_data
+
+
+device = torch.device("cuda")
+
+net_params = snn.params(args.network_config)
+net = SumPool(net_params).to(device)
+net2 = AvgPool().to(device)
+
+tact_arr = []
+
+tact_count = len(glob.glob(str(Path(args.save_path) / "*_tact.npy")))
+print(f"Processing {tact_count} tactile files...")
+
+for i in range(tact_count):
+    print(f"Processing tactile {i}...")
+    # tactile
+    tact_npy = Path(args.save_path) / f"{i}_tact.npy"
+    tact = torch.FloatTensor(np.load(tact_npy))
+    tact_arr.append(tact)
+
+tact = torch.stack(tact_arr)
+torch.save(tact, Path(args.save_path) / "tact.pt")
+
+print(f"tact shape: {tact.shape}")
+print("Generating rectangular tactile data...")
+# Convert tactile input into rectangular format using an assignment rule (for CNN)
+N, _, _, T = tact.shape
+
+
+def convert_to_rectangle(a):
+    img_depth_list = []
+    for i in range(a.shape[-1]):
+        img_depth_list.append(
+            [
+                [
+                    a[3, i],
+                    (a[3, i] + a[10, i] + a[6, i]) / 3,
+                    a[10, i],
+                    (a[10, i] + a[29, i] + a[14, i]) / 3,
+                    a[24, i],
+                    (a[24, i] + a[29, i] + a[34, i]) / 3,
+                    a[34, i],
+                ],
+                [
+                    (a[3, i] + a[1, i] + a[6, i]) / 3,
+                    a[6, i],
+                    (a[6, i] + a[10, i] + a[11, i] + a[17, i] + a[19, i]) / 5,
+                    a[17, i],
+                    (a[24, i] + a[28, i] + a[29, i] + a[17, i] + a[19, i]) / 5,
+                    a[29, i],
+                    (a[29, i] + a[34, i] + a[36, i]) / 3,
+                ],
+                [
+                    a[1, i],
+                    (a[1, i] + a[6, i] + a[8, i] + a[11, i] + a[15, i]) / 5,
+                    a[11, i],
+                    a[19, i],
+                    a[28, i],
+                    (a[22, i] + a[28, i] + a[29, i] + a[36, i] + a[32, i]) / 5,
+                    a[36, i],
+                ],
+                [
+                    (a[0, i] + a[1, i] + a[5, i] + a[8, i]) / 4,
+                    a[8, i],
+                    a[15, i],
+                    (
+                        a[11, i]
+                        + a[15, i]
+                        + a[14, i]
+                        + a[20, i]
+                        + a[26, i]
+                        + a[22, i]
+                        + a[28, i]
+                        + a[19, i]
+                    )
+                    / 8,
+                    a[22, i],
+                    a[32, i],
+                    (a[32, i] + a[33, i] + a[38, i] + a[36, i]) / 4,
+                ],
+                [
+                    a[0, i],
+                    a[5, i],
+                    a[14, i],
+                    a[20, i],
+                    a[26, i],
+                    a[33, i],
+                    a[38, i],
+                ],
+                [
+                    (a[0, i] + a[2, i] + a[5, i] + a[9, i]) / 4,
+                    a[9, i],
+                    a[16, i],
+                    (
+                        a[14, i]
+                        + a[16, i]
+                        + a[13, i]
+                        + a[21, i]
+                        + a[27, i]
+                        + a[23, i]
+                        + a[26, i]
+                        + a[20, i]
+                    )
+                    / 8,
+                    a[23, i],
+                    a[30, i],
+                    (a[33, i] + a[30, i] + a[38, i] + a[37, i]) / 4,
+                ],
+                [
+                    a[2, i],
+                    (a[2, i] + a[7, i] + a[9, i] + a[13, i] + a[16, i]) / 5,
+                    a[13, i],
+                    a[21, i],
+                    a[27, i],
+                    (a[23, i] + a[27, i] + a[31, i] + a[37, i] + a[30, i]) / 5,
+                    a[37, i],
+                ],
+                [
+                    (a[2, i] + a[7, i] + a[4, i]) / 3,
+                    a[7, i],
+                    (a[12, i] + a[13, i] + a[7, i] + a[18, i] + a[21, i]) / 5,
+                    a[18, i],
+                    (a[21, i] + a[18, i] + a[25, i] + a[31, i] + a[27, i]) / 5,
+                    a[31, i],
+                    (a[31, i] + a[35, i] + a[37, i]) / 3,
+                ],
+                [
+                    a[4, i],
+                    (a[4, i] + a[7, i] + a[18, i]) / 3,
+                    a[12, i],
+                    (a[12, i] + a[18, i] + a[25, i]) / 3,
+                    a[25, i],
+                    (a[31, i] + a[25, i] + a[35, i]) / 3,
+                    a[35, i],
+                ],
+            ]
+        )
+    return img_depth_list
+
+
+all_data_l = np.zeros([N, 2, 9, 7, T])
+all_data_r = np.zeros([N, 2, 9, 7, T])
+
+
+for k in range(N):
+    print(f"Processing tactile {k}...")
+    sample_file = tact[k, ...]
+    channel_list_left = []
+    channel_list_right = []
+    for channel in range(2):
+        a = sample_file[:, channel, :]
+        img_depth_right = convert_to_rectangle(a[:39])
+        img_depth_left = convert_to_rectangle(a[39:])
+        img_depth = img_depth_right + img_depth_left
+        channel_list_left.append(img_depth_left)
+        channel_list_right.append(img_depth_right)
+
+    ri_left = np.array(channel_list_left)
+    ri_left[ri_left > 0.5] = 1
+    ri_left[ri_left <= 0.5] = 0
+    ri_left = ri_left.swapaxes(2, 1).swapaxes(2, 3).astype(float)
+
+    # ri_left = torch.FloatTensor(ri_left)
+    all_data_l[k] = ri_left
+
+    ri_right = np.array(channel_list_right)
+    ri_right[ri_right > 0.5] = 1
+    ri_right[ri_right <= 0.5] = 0
+
+    ri_right = ri_right.swapaxes(2, 1).swapaxes(2, 3).astype(float)
+    # ri_right = torch.FloatTensor(ri_left)
+    all_data_r[k] = ri_right
+
+
+all_data_r = torch.FloatTensor(all_data_r.astype(float))
+all_data_l = torch.FloatTensor(all_data_l.astype(float))
+
+print(f"right rectangular tactile : {all_data_r.shape}")
+print(f"left rectangular tactile : {all_data_l.shape}")
+
+torch.save(all_data_r, Path(args.save_path) / "tac_right.pt")
+torch.save(all_data_l, Path(args.save_path) / "tac_left.pt")
+
+print("Done processing tactile.")
+
+del tact
+del all_data_r
+del all_data_l
+
+ds_vis_spike_arr = []
+ds_vis_non_spike_arr = []
+
+vis_count = len(glob.glob(str(Path(args.save_path) / "*_vis.npy")))
+print(f"Processing {vis_count} vision files...")
+
+for i in range(vis_count):
+
+    print(f"Processing vision {i}...")
+    vis_npy = Path(args.save_path) / f"{i}_vis.npy"
+    vis = torch.FloatTensor(np.load(vis_npy)).unsqueeze(0)
+    vis = vis.to(device)
+
+    with torch.no_grad():
+        vis_pooled_spike = net.forward(vis)
+        vis_pooled_non_spike = net2.forward(vis.permute(0, 1, 4, 2, 3))
+
+    ds_vis_spike_arr.append(vis_pooled_spike.detach().cpu().squeeze(0))
+    ds_vis_non_spike_arr.append(
+        vis_pooled_non_spike.squeeze(0).permute(0, 2, 3, 1).detach().cpu()
+    )
+
+ds_vis_spike = torch.stack(ds_vis_spike_arr)
+print(f"ds_vis: {ds_vis_spike.shape}")
+torch.save(ds_vis_spike, Path(args.save_path) / "ds_vis.pt")
+del ds_vis_spike, ds_vis_spike_arr
+
+ds_vis_non_spike = torch.stack(ds_vis_non_spike_arr)
+print(f"ds_vis_non_spike: {ds_vis_non_spike.shape}")
+torch.save(ds_vis_non_spike, Path(args.save_path) / "ds_vis_non_spike.pt")
