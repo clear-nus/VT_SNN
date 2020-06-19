b3c2792d2c0c0a9ab25e8285ca2fa5ac87f2983e
Jethro Kuan <jethrokuan95@gmail.com>
2020-06-19 20:08:40 +0800

    Add loihi training for sd

--- vtsnn/models/loihi.py
+++ vtsnn/models/loihi.py
@@ -1,25 +1,125 @@
+"""SNN models compatible with the Loihi."""
+
+from pathlib import Path
+import numpy as np
 import torch
 import slayerSNN as snn
 from slayerSNN import loihi as spikeLayer
 
 
 class SlayerLoihiMLP(torch.nn.Module):
-    """2-layer MLP based on SLAYER used for tactile data. To be trained and run on the Loihi."""
+    """
+    2 layer MLP based on SLAYER, using the Loihi neuron model.
+    """
 
-    def __init__(self, params, input_size, hidden_size, output_size):
+    def __init__(
+        self, params, input_size, hidden_size, output_size, quantize=True
+    ):
         super(SlayerLoihiMLP, self).__init__()
         self.output_size = output_size
+        self.quantize = quantize
         self.slayer = spikeLayer(params["neuron"], params["simulation"])
-        self.fc1 = self.slayer.dense(input_size, hidden_size)
-        self.fc2 = self.slayer.dense(hidden_size, output_size)
-        self.spike_trains = None
+        self.fc1 = self.slayer.dense(
+            input_size, hidden_size, quantize=self.quantize
+        )
+        self.fc2 = self.slayer.dense(
+            hidden_size, output_size, quantize=self.quantize
+        )
 
     def forward(self, spike_input):
-        spike_1 = self.slayer.spikeLoihi(self.slayer.psp(self.fc1(spike_input)))
+        spike_1 = self.slayer.spikeLoihi(self.fc1(spike_input))
         spike_1 = self.slayer.delayShift(spike_1, 1)
-        spike_output = self.slayer.spikeLoihi(
-            self.slayer.psp(self.fc2(spike_1))
-        )
+        spike_output = self.slayer.spikeLoihi(self.fc2(spike_1))
         spike_output = self.slayer.delayShift(spike_output, 1)
         self.spike_trains = [spike_1]
         return spike_output
+
+    def quantize(self, epoch, checkpoint_dir):
+        fc1_weights = (
+            snn.utils.quantize(self.fc1.weight, 2)
+            .flatten()
+            .cpu()
+            .data.numpy()
+            .reshape(self.hidden_size, -1)
+        )
+        fc2_weights = (
+            snn.utils.quantize(self.fc2.weight, 2)
+            .flatten()
+            .cpu()
+            .data.numpy()
+            .reshape(args.output_size, -1)
+        )
+
+        np.save(
+            Path(checkpoint_dir) / f"{epoch:03d}-fc1.npy", fc1_weights,
+        )
+        np.save(
+            Path(checkpoint_dir) / f"{epoch:03d}-fc2.npy", fc2_weights,
+        )
+
+
+class SlayerLoihiMM(torch.nn.Module):
+    def __init__(
+        self,
+        params,
+        tact_input_size,
+        vis_input_size,
+        tact_output_size,
+        vis_output_size,
+        output_size,
+    ):
+        super(SlayerLoihiMM, self).__init__()
+        self.tact_output_size = tact_output_size
+        self.vis_output_size = vis_output_size
+        self.output_size = output_size
+        self.slayer = spikeLayer(params["neuron"], params["simulation"])
+
+        self.tact_fc = self.slayer.dense(tact_input_size, tact_output_size)
+        self.vis_fc = self.slayer.dense(vis_input_size, vis_output_size)
+        self.combi = self.slayer.dense(
+            tact_output_size + vis_output_size, output_size
+        )
+
+    def forward(self, spikeInputTact, spikeInputVis):
+        tact_int = self.slayer.spikeLoihi(self.tact_fc(spikeInputTact))
+        tact_int = self.slayer.delayShift(tact_int, 1)
+        vis_int = self.slayer.spikeLoihi(self.vis_fc(spikeInputVis))
+        vis_int = self.slayer.delayShift(vis_int, 1)
+        combi = torch.cat([tact_int, vis_int], dim=1)
+        out = self.slayer.spikeLoihi(self.combi(combi))
+        out = self.slayer.delayShift(out, 1)
+
+        return out
+
+    def quantize(self, epoch, checkpoint_dir):
+        tact_fc_weights = (
+            snn.utils.quantize(self.tact_fc.weight, 2)
+            .flatten()
+            .cpu()
+            .data.numpy()
+            .reshape(self.tact_output_size, -1)
+        )
+        vis_fc_weights = (
+            snn.utils.quantize(self.vis_fc.weight, 2)
+            .flatten()
+            .cpu()
+            .data.numpy()
+            .reshape(self.vis_output_size, -1)
+        )
+        combi_weights = (
+            snn.utils.quantize(self.combi.weight, 2)
+            .flatten()
+            .cpu()
+            .data.numpy()
+            .reshape(self.output_size, -1)
+        )
+
+        np.save(
+            Path(checkpoint_dir) / f"{epoch:03d}-tact.npy", tact_fc_weights,
+        )
+        np.save(
+            Path(checkpoint_dir) / f"{epoch:03d}-vis.npy", vis_fc_weights,
+        )
+        np.save(
+            Path(checkpoint_dir) / f"{epoch:03d}-combi.npy", combi_weights,
+        )
---
+++ vtsnn/train_loihi.py
@@ -0,0 +1,224 @@
+"""Train the VT-SNN models
+
+1. With guild:
+guild run vtsnn-{tact,vis,mm}:train-{cw,sd}
+
+2. With vanilla Python:
+
+python vtsnn/train_loihi.py \
+ --epochs 500 \
+ --lr 0.001 \
+ --sample_file 1 \
+ --batch_size 8 \
+ --network_config network_config/correct_config.yml \
+ --data_dir /path/to/preprocessed \
+ --hidden_size 32 \
+ --loss NumSpikes \
+ --mode tact \
+ --task cw
+
+where mode is one of {tact, vis, mm} and task is {cw, slip}.
+"""
+from pathlib import Path
+import logging
+import argparse
+
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+import slayerSNN as snn
+
+from slayerSNN import loihi as spikeLayer
+from vtsnn.models.loihi import SlayerLoihiMLP, SlayerLoihiMM
+from vtsnn.dataset import ViTacDataset
+
+logging.basicConfig(level=logging.INFO, format="%(levelname)s %(message)s")
+log = logging.getLogger()
+
+parser = argparse.ArgumentParser("Train VT-SNN models.")
+
+parser.add_argument(
+    "--epochs", type=int, help="Number of epochs.", required=True
+)
+parser.add_argument("--data_dir", type=str, help="Path to data.", required=True)
+parser.add_argument(
+    "--checkpoint_dir",
+    type=str,
+    help="Path for saving checkpoints.",
+    default=".",
+)
+parser.add_argument(
+    "--network_config",
+    type=str,
+    help="Path SNN network configuration.",
+    required=True,
+)
+parser.add_argument("--lr", type=float, help="Learning rate.", required=True)
+parser.add_argument(
+    "--sample_file",
+    type=int,
+    help="Sample number to train from.",
+    required=True,
+)
+parser.add_argument(
+    "--hidden_size", type=int, help="Size of hidden layer.", required=True
+)
+parser.add_argument(
+    "--mode",
+    type=str,
+    choices=["tact", "vis", "mm"],
+    help="Type of model to run.",
+    required=True,
+)
+
+parser.add_argument(
+    "--task",
+    type=str,
+    help="The classification task.",
+    choices=["slip"],  # Loihi can't handle > 16 classes
+    default="slip",
+)
+
+parser.add_argument("--batch_size", type=int, help="Batch Size.", required=True)
+
+parser.add_argument(
+    "--loss",
+    type=str,
+    help="Loss function to use.",
+    choices=["NumSpikes", "WeightedNumSpikes"],
+    required=True,
+)
+args = parser.parse_args()
+
+LOSS_TYPES = ["NumSpikes", "WeightedNumSpikes"]
+
+params = snn.params(args.network_config)
+
+if args.task == "cw":
+    output_size = 20
+else:  # Slip
+    output_size = 2
+
+if args.mode == "tact":
+    model = SlayerLoihiMLP
+    model_args = {
+        "params": params,
+        "input_size": 156,
+        "hidden_size": args.hidden_size,
+        "output_size": output_size,
+    }
+elif args.mode == "vis":
+    model = SlayerLoihiMLP
+    model_args = {
+        "params": params,
+        "input_size": (50, 63, 2),
+        "hidden_size": args.hidden_size,
+        "output_size": output_size,
+    }
+else:  # NOTE: args.hidden_size unused here
+    model = SlayerLoihiMM
+    model_args = {
+        "params": params,
+        "tact_input_size": 156,
+        "vis_input_size": (50, 63, 2),
+        "tact_output_size": 50,
+        "vis_output_size": 10,
+        "output_size": output_size,
+    }
+
+device = torch.device("cuda")
+writer = SummaryWriter(".")
+net = model(**model_args).to(device)
+
+
+if args.loss == "NumSpikes":
+    params["training"]["error"]["type"] = "NumSpikes"
+    error = snn.loss(params, spikeLayer).to(device)
+    criteria = error.numSpikes
+elif args.loss == "WeightedNumSpikes":
+    params["training"]["error"]["type"] = "WeightedNumSpikes"
+    error = snn.loss(params, spikeLayer).to(device)
+    criteria = error.weightedNumSpikes
+
+optimizer = torch.optim.RMSprop(net.parameters(), lr=args.lr, weight_decay=0.5)
+
+train_dataset = ViTacDataset(
+    path=args.data_dir,
+    sample_file=f"train_80_20_{args.sample_file}.txt",
+    output_size=output_size,
+    spiking=True,
+    mode=args.mode,
+)
+train_loader = DataLoader(
+    dataset=train_dataset,
+    batch_size=args.batch_size,
+    shuffle=True,
+    num_workers=8,
+)
+test_dataset = ViTacDataset(
+    path=args.data_dir,
+    sample_file=f"test_80_20_{args.sample_file}.txt",
+    output_size=output_size,
+    spiking=True,
+    mode=args.mode,
+)
+test_loader = DataLoader(
+    dataset=test_dataset,
+    batch_size=args.batch_size,
+    shuffle=False,
+    num_workers=8,
+)
+
+
+def _train():
+    correct = 0
+    num_samples = 0
+    net.train()
+    for *data, target, label in train_loader:
+        data = [d.to(device) for d in data]
+        target = target.to(device)
+        output = net.forward(*data)
+        correct += torch.sum(snn.predict.getClass(output) == label).data.item()
+        num_samples += len(label)
+        spike_loss = criteria(output, target)
+
+        optimizer.zero_grad()
+        spike_loss.backward()
+        optimizer.step()
+
+    writer.add_scalar("loss/train", spike_loss / len(train_loader), epoch)
+    writer.add_scalar("acc/train", correct / num_samples, epoch)
+
+
+def _test():
+    correct = 0
+    num_samples = 0
+    net.eval()
+    with torch.no_grad():
+        for *data, target, label in test_loader:
+            data = [d.to(device) for d in data]
+            target = target.to(device)
+            output = net.forward(*data)
+            correct += torch.sum(
+                snn.predict.getClass(output) == label
+            ).data.item()
+            num_samples += len(label)
+            spike_loss = criteria(output, target)  # numSpikes
+
+        writer.add_scalar("loss/test", spike_loss / len(test_loader), epoch)
+        writer.add_scalar("acc/test", correct / num_samples, epoch)
+
+
+def _save_model(epoch):
+    log.info(f"Writing model at epoch {epoch}...")
+    checkpoint_path = Path(args.checkpoint_dir) / f"weights_{epoch:03d}.pt"
+    torch.save(net.state_dict(), checkpoint_path)
+
+
+for epoch in range(1, args.epochs + 1):
+    _train()
+    if epoch % 10 == 0:
+        _test()
+    if epoch % 100 == 0:
+        _save_model(epoch)
+        net.quantize(epoch, args.checkpoint_dir)
