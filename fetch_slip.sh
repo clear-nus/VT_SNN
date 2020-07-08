7432ff611fb4e7abd956ef8a3003fb93254f3277
tasbolat1 <ttaunyazov@nu.edu.kz>
2020-07-08 21:56:31 +0800

    Update fetch_slip.sh
    fetch_slip.sh is changed to download preprocessed slip data

--- fetch_slip.sh
+++ fetch_slip.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./gdrivedl.py https://drive.google.com/file/d/1VBCwDNwjRqRMQ4iPHo8WRh9n5g92nzt6/view?usp=sharing
+#./gdrivedl.py https://drive.google.com/file/d/1VBCwDNwjRqRMQ4iPHo8WRh9n5g92nzt6/view?usp=sharing
+
+# this downloads the preprocessed slip data
+./gdrivedl.py https://drive.google.com/file/d/1ODFWBdCMq6NT1O4jDI1zxBc-3flF8U7h/view?usp=sharing
